89b6f0c872c4a582f08de69aece00b532eca83b6
SLJ <18234175208@163.com>
2018-11-01 16:58:17 +0800

    使用虚拟头节点dummyHead，消除add操作index为0时的特殊处理

--- src/链表/LinkedList.java
+++ src/链表/LinkedList.java
@@ -1,5 +1,10 @@
 package 链表;
 
+/**
+*@Description: 自定义实现链表
+*@create: 2018/11/1
+*@Author: SLJ
+*/
 public class LinkedList<E> {
 
     //链表的节点类
@@ -24,11 +29,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;//头节点
+    private Node dummyHead;//头节点
     private int size;//元素个数
 
     public LinkedList(){
-        head = null;
+        dummyHead = new Node(null,null);
         size = 0;
     }
 
@@ -40,12 +45,6 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    //链表头部添加元素e
-    public void addFirst(E e){
-        head = new Node(e,head);
-        size ++;
-    }
-
     //链表指定位置添加元素e
     public void add(int index,E e){
 
@@ -53,18 +52,19 @@ public class LinkedList<E> {
             throw new IllegalArgumentException("index 需要满足 index>0 and index<size");
         }
 
-        if (index == 0){
-            addFirst(e);
-        }else {
-            Node prev = head;
-            for (int i = 0; i < index-1; i++) {
-                prev = prev.next;
-            }
-            prev.next = new Node(e,prev.next);
-            size ++;
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
         }
-
+        prev.next = new Node(e,prev.next);
+        size ++;
     }
+
+    //链表头部添加元素e
+    public void addFirst(E e){
+        add(0,e);
+    }
+
 
     //链表尾部添加元素e
     public void addLast(E e){
